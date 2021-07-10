d6f531f7a2696775e4e51aca20f140d5e60fab73
Anup Aggarwal <anupaggarwal75@gmail.com>
2021-07-10 19:23:37 +0530

    Delete redundant lines

--- StockPrice_live.py
+++ StockPrice_live.py
@@ -9,30 +9,12 @@ import datetime
 def PriceTracker():
     r = requests.get('https://finance.yahoo.com/quote/GOOG?p=GOOG&.tsrc=fin-srch').text
     soup = BeautifulSoup(r, 'lxml')
-    # print(soup.prettify())
     match1 = soup.find('div', class_='D(ib) Mend(20px)')
-    # print(match1)
     match2 = match1.span.text
     return match2
-# timeStamp = datetime.datetime.now()
-# timeStamp = timeStamp.strftime("%Y-%m-%d %H:%M:%S")
 while True:
     print(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Current stock price of GOOG is ${PriceTracker()}')
-    # print(f'{timeStamp} Current stock price of GOOG is ${PriceTracker()}')
-
-
-# Alternatively, we can use the find_all method:
-
-# import requests
-# from bs4 import BeautifulSoup
-# r = requests.get('https://finance.yahoo.com/quote/GOOG?p=GOOG&.tsrc=fin-srch').text
-# soup = BeautifulSoup(r, 'lxml')
-# # print(soup.prettify())
-# match1 = soup.find_all('div', class_='D(ib) Mend(20px)')
-# # print(match1[0])
-# match2 = match1[0].span.text
-# print(f'Current stock price of GOOG is ${match2}')
-
+    
 
  
 
